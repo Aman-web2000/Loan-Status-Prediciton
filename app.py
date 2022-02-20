9785ff6349c863886d4a2f952993ec8e6357dc0f
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2022-02-20 12:41:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-import streamlit as st
+import streamlit as st 
 import numpy as np
-import pandas as pd 
-import seaborn as sns 
+import pandas as pd
 import pickle
 from PIL import Image
 
-
-pickle_in=open('LoanPrediction.pkl','rb')
+pickle_in=open('Titanic.pkl','rb')
 clf=pickle.load(pickle_in)
+
+def predictor(l):
+    prediction=clf.predict([l])
+    return prediction[0]
 
 ## Background Image
 st.markdown(
          f"""
          <style>
          .stApp {{
-             background: url("https://images.unsplash.com/photo-1583574928052-9a2563277468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80");
+             background: url("https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2016%2F09%2FRussian-Wreck-Zabagad-SHIPS0816-2000.jpg");
              background-size: cover
          }}
          </style>
@@ -22,87 +24,116 @@ st.markdown(
          unsafe_allow_html=True
      )
 
-def predictor(l):
-    prediction=clf.predict([l])
-    return prediction[0]
 
+# Side bar Design 
+
+html_sidebar_heading="""
+<div style='background-color:#0b2038;'>
+<h1 style="color: white; text-align: center">Prediction</h1>
+</div>
+"""
+
+st.sidebar.markdown(html_sidebar_heading,unsafe_allow_html=True)
+
+st.sidebar.subheader('Enter the Data Below :')
+
+Pclass= st.sidebar.selectbox("Passenger Class",['1','2','3'])
+Age= st.sidebar.slider("Age",min_value=2,max_value=80,step=2)
+fare= st.sidebar.slider("Fare",min_value=10,max_value=1000,step=40)
+no_of_family_members= st.sidebar.slider("No of Fmaily members on ship",min_value=0,max_value=20,step=1)
+sex= st.sidebar.radio('Sex',['Male','Female'])
+Married= st.sidebar.radio("Married",['Yes',"No"])
+Embarked= st.sidebar.selectbox("Embarked",['S','C','Q'])
+
+l=[Pclass,Age,fare,no_of_family_members,sex,Married,Embarked]
+
+# Main Page 
 
 def main():
-    html_title="""
-    <div style="background-color: darkslategrey; padding:10 px; border:double">
-    <h1 style="color: white; font: bolder; text-align: center">Loan Status Predictor</h1>
+
+    html_page_title="""
+    <div style="background-color: red;">
+    <h1 style="text-align: center; color: white;">Titanic Survival Prediction</h1>
     </div>
     """
-    st.markdown(html_title,unsafe_allow_html=True)
+
+    st.markdown(html_page_title,unsafe_allow_html=True)
+
+    image_path='https://digitaldomain.com/wp-content/uploads/2019/09/Titanic_2.jpg'
+    st.image(image_path)
     
-    ApplicantIncome=st.number_input('Enter your Income',min_value=10000,max_value=10000000,step=5000)
-    CoApplicantIncome=st.number_input('Enter CoApplicant Income',min_value=5000,max_value=1000000,step=2000)
-    LoanAmount=st.slider('Loan Amount',min_value=50000,max_value=1000000,step=5000)
-    LoanAmountTerm=st.number_input('Loan Amount Term',min_value=2,max_value=12,step=1)
-    CreditHistory=st.radio('Do You Own Credit Card :',['0','1'])
-    Dependents=st.radio('No of Dependents :',['0','1','2','3+'])
-    propertyType=st.selectbox('Property Type',['Rural','Semi Urban','Urban'])
-    Education=st.selectbox('Education Qualification',['Graduate','Non Graduate'])
-    Employed=st.radio('Are you Self Employed:',['Yes','No'])
+    html_about='''<div style="background-color: black; padding: 10px;">
+    <p style="text-align: center; color: aliceblue;">The sinking of the Titanic is one of the most infamous shipwrecks in history.
+    <br>
+    <br>
+    On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS 
+    Titanic sank after colliding with an iceberg. Unfortunately, 
+    there weren’t enough lifeboats for everyone onboard, 
+    resulting in the death of 1502 out of 2224 passengers and crew.
+    <br>
+    <br>
+    While there was some element of luck involved in surviving, it seems some 
+    groups of people were more likely to survive than others.
+    <br>
+    <br>
+    This Model predicts whether you would have survived this tragedy or not based on the data that you 
+    enter in the model .
+    <br>
+    <br>
+    Good Luck!</p>
+    </div>'''
 
-    l=[ApplicantIncome,CoApplicantIncome,LoanAmount,LoanAmountTerm,CreditHistory,Dependents,propertyType]
-    l1=[]
-
-    l1.append(l[0])
-    l1.append(l[1])
-    l1.append(l[2])
-    l1.append(l[3])
-
-    if l[4]=='0':
-        l1.append(0)
-    elif l[4]=='1':
-        l1.append(1)
-
-    if l[5]=='0':
-        l1.append(0)
-        l1.append(0)
-        l1.append(0)
-    elif l[5]=='1':
-        l1.append(1)
-        l1.append(0)
-        l1.append(0)
-    elif l[5]=='2':
-        l1.append(0)
-        l1.append(1)
-        l1.append(0)
-    elif l[5]=='3+':
-        l1.append(0)
-        l1.append(0)
-        l1.append(1)
-        
-    if l[-1]=='Rural':
-        l1.append(0)
-        l1.append(0)
-    elif l[-1]=='Semi Urban':
-        l1.append(1)
-        l1.append(0)
-    elif l[-1]=='Urban':
-        l1.append(0)
-        l1.append(1)
+    st.markdown(html_about,unsafe_allow_html=True)
 
 
-    if st.button('Predict'):
+    if st.sidebar.button("Predict"):
+            if l[0]=='1':
+                l[0]=3
+            elif l[0]=='3':
+                l[0]=1
+            elif l[0]=='2':
+                l[0]=2
 
-        ans=predictor(l1)
-        if ans==1:
-            win="""
-            <div style="background-color:#FFC300 ; padding:10px">
-            <h2 style="color:black,font:bolder;">"Congratulations , Your Loan Application has been Approved!"</h2>
-            </div>
-            """
-            st.markdown(win,unsafe_allow_html=True)
-        else:
-            lose="""
-            <div style="background-color:#C70039 ; padding:10px">
-            <h2 style="color:aliceblue ;font:bolder;">Sorry , Your Loan Application has been Rejected !</h2>
-            </div>
-            """
-            st.markdown(lose,unsafe_allow_html=True)
+            if l[4]=='Male':
+                l[4]=1
+            elif l[4]=='Female':
+                l[4]=0
+    
+            if l[5]=='Yes':
+                l[5]=1
+            elif l[5]=='No':
+                l[5]=0
+    
 
-if __name__=='__main__':
+            if l[-1]=='C':
+                del l[-1]
+                l.append(0)
+                l.append(0)
+            elif l[-1]=='Q':
+                del l[-1]
+                l.append(1)
+                l.append(0)
+            elif l[-1]=='S':
+                del l[-1]
+                l.append(0)
+                l.append(1)
+            
+            ans=predictor(l)
+            
+            if ans==1:
+                survive="""
+                <div style="background-color: #4508EF;padding: 10px;">
+                <h1 style="text-align: center; color: white;">Hurrah! 👏 You have Survived the Titanic</h1>
+                </div>
+                """
+                st.markdown(survive,unsafe_allow_html=True)
+            else:
+                not_survived="""
+                <div style="background-color: #900C3F; padding: 10px ">
+                <h1 style="color: White; text-align: center;">R.I.P ☠️</h1>
+                </div>
+                """
+                st.markdown(not_survived,unsafe_allow_html=True)
+
+if __name__=="__main__":
     main()
